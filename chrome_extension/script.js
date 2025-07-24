36c182d9ee3053c22e727ddfea6aa8324c121550
jorge-antonio-gomez <jorgeantoniodarkman@gmail.com>
2025-07-23 23:54:58 -0600

    feat: Mejoras en UI del toolbar y editor TipTap
    - Reemplazados iconos de texto por iconos SVG en toolbar TipTap
    - Añadidos recursos de iconos al manifest.json (images/toolbar-icons/*)
    - Mejorado diseño del botón delete con mejor especificidad CSS
    - Reorganizado estilos del editor ProseMirror para mejor estructura
    - Añadida sombra y mejores espaciados al editor
    - Mejorados estilos del modal de enlaces con backgrounds sutiles
    - Añadida consistencia visual en placeholders y elementos de UI

--- chrome_extension/script.js
+++ chrome_extension/script.js
@@ -84,16 +84,16 @@
 
     function buildToolbar(toolbarContainer, editor, lang) {
         toolbarContainer.innerHTML = `
-                    <button data-action="bold" title="${lang.toolbarBold}"><strong>B</strong></button>
-                    <button data-action="italic" title="${lang.toolbarItalic}"><em>I</em></button>
-                    <button data-action="underline" title="${lang.toolbarUnderline}"><u>U</u></button>
-                    <button data-action="strike" title="${lang.toolbarStrike}"><s>S</s></button>
-                    <button data-action="blockquote" title="${lang.toolbarBlockquote}"><span style="font-size: 1.3em; font-weight: bold;">"</span></button>
-                    <button data-action="codeBlock" title="${lang.toolbarCodeBlock}"><strong>{ }</strong></button>
-                    <button data-action="link" title="${lang.toolbarLink}">🔗</button>
-                    <button data-action="h1" title="${lang.heading} 1">h1</button>
-                    <button data-action="h2" title="${lang.heading} 2">h2</button>
-                    <button data-action="h3" title="${lang.heading} 3">h3</button>
+                    <button data-action="bold" title="${lang.toolbarBold}"><img src="${chrome.runtime.getURL('images/toolbar-icons/bold.svg')}" style="max-height: 14px; max-width: 14px; height: 14px; width: 14px;"></button>
+                    <button data-action="italic" title="${lang.toolbarItalic}"><img src="${chrome.runtime.getURL('images/toolbar-icons/italic.svg')}" style="max-height: 14px; max-width: 14px; height: 14px; width: 14px;"></button>
+                    <button data-action="underline" title="${lang.toolbarUnderline}"><img src="${chrome.runtime.getURL('images/toolbar-icons/underline.svg')}" style="max-height: 14px; max-width: 14px; height: 14px; width: 14px;"></button>
+                    <button data-action="strike" title="${lang.toolbarStrike}"><img src="${chrome.runtime.getURL('images/toolbar-icons/strikethrough.svg')}" style="max-height: 14px; max-width: 14px; height: 14px; width: 14px;"></button>
+                    <button data-action="blockquote" title="${lang.toolbarBlockquote}"><img src="${chrome.runtime.getURL('images/toolbar-icons/chat-left.svg')}" style="max-height: 14px; max-width: 14px; height: 14px; width: 14px;"></button>
+                    <button data-action="codeBlock" title="${lang.toolbarCodeBlock}"><img src="${chrome.runtime.getURL('images/toolbar-icons/bracket.svg')}" style="max-height: 14px; max-width: 14px; height: 14px; width: 14px;"></button>
+                    <button data-action="link" title="${lang.toolbarLink}"><img src="${chrome.runtime.getURL('images/toolbar-icons/link-alt.svg')}" style="max-height: 14px; max-width: 14px; height: 14px; width: 14px;"></button>
+                    <button data-action="h1" title="${lang.heading} 1"><img src="${chrome.runtime.getURL('images/toolbar-icons/heading-1.svg')}" style="max-height: 14px; max-width: 14px; height: 14px; width: 14px;"></button>
+                    <button data-action="h2" title="${lang.heading} 2"><img src="${chrome.runtime.getURL('images/toolbar-icons/heading-2.svg')}" style="max-height: 14px; max-width: 14px; height: 14px; width: 14px;"></button>
+                    <button data-action="h3" title="${lang.heading} 3"><img src="${chrome.runtime.getURL('images/toolbar-icons/heading-3.svg')}" style="max-height: 14px; max-width: 14px; height: 14px; width: 14px;"></button>
                 `;
 
         toolbarContainer.querySelectorAll('button').forEach(button => {
